6aa93ba1a3f525a51ec39d867f94f72ffa956cf1
mauro-balades <mauro.balades@tutanota.com>
2022-09-21 07:57:51 +0200

    added a counter

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+window.onload = () => {
+    const RESULT_ELEMENT = document.getElementById("result");
+    var has_new_js_framework = true;
+
+    setInterval(function () {
+        if (Math.random()>0.5) {
+            has_new_js_framework = !has_new_js_framework;
+        } else {
+            has_new_js_framework++;
+        }
+
+        RESULT_ELEMENT.innerHTML = `${+has_new_js_framework}s`
+    }, 1000);
+}
