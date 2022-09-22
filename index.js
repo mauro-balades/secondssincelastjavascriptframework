53495b9d5f97b0d99e7e1be2fdfcda76dfaa67c7
mauro-balades <mauro.balades@tutanota.com>
2022-09-22 17:22:29 +0200

    Revert "Made it a fixed timer to improve performance."
    This reverts commit 20fb520f4bcfe09b39eecf6f3053b6b837ca0481.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+
+
 window.onload = () => {
     const RESULT_ELEMENT = document.getElementById("result");
+<<<<<<< HEAD
     var time_counter = 0;
     let time_to_next = 10;
     
@@ -12,6 +15,15 @@ window.onload = () => {
             time_counter = 0;
         } else{
             time_counter++;
+=======
+    var has_new_js_framework = true;
+
+    setInterval(function () {
+        if (Math.random()>0.5) {
+            has_new_js_framework = !has_new_js_framework;
+        } else {
+            has_new_js_framework++;
+>>>>>>> parent of 20fb520... Made it a fixed timer to improve performance.
         }
 
         RESULT_ELEMENT.innerHTML = `${+time_counter}s`
