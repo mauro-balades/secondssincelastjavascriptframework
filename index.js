8088c8c7b3da5af74cdf8e7b2c0902b77f94d90b
mauro-balades <mauro.balades@tutanota.com>
2022-09-22 17:23:54 +0200

    fixed git diff

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-
-
 window.onload = () => {
     const RESULT_ELEMENT = document.getElementById("result");
-<<<<<<< HEAD
-    var time_counter = 0;
-    let time_to_next = 10;
-    
-    const MAX_TIME = 10;
-
-    setInterval(function () {
-
-        if(time_counter >= time_to_next){
-            time_to_next = Math.random() * MAX_TIME;
-            time_counter = 0;
-        } else{
-            time_counter++;
-=======
     var has_new_js_framework = true;
 
     setInterval(function () {
@@ -23,9 +7,8 @@ window.onload = () => {
             has_new_js_framework = !has_new_js_framework;
         } else {
             has_new_js_framework++;
->>>>>>> parent of 20fb520... Made it a fixed timer to improve performance.
         }
 
-        RESULT_ELEMENT.innerHTML = `${+time_counter}s`
+        RESULT_ELEMENT.innerHTML = `${+has_new_js_framework}s`
     }, 1000);
 }
